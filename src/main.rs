5c7872e76094af4ff97f272f8f677bfa62a6d8f5
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-10 18:59:08 -0300

    feat(src): Added boiler plate for base routing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http;
+use axum::routing::{get, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port = 3000;
+    let address = format!("0.0.0.0:{}", port);
+
+    let app = Router::new().route("/", get(health));
+
+    axum::Server::bind(&address.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn health() -> http::StatusCode {
+    http::StatusCode::OK
 }
