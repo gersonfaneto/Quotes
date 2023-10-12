bfc83934e39192b088c22a1d565402ec5bc9316f
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-12 19:33:42 -0300

    feat: Added connection for the DB.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,22 @@ mod handlers;
 mod models;
 
 use axum::routing::{get, Router};
+use sqlx::postgres::PgPoolOptions;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let address = format!("0.0.0.0:{}", port);
+
+    let database_url = env::var("DATABASE_URL").expect("Missing enviromet variable - DATABASE_URL");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .unwrap();
+
     let app = Router::new().route("/", get(handlers::get::health));
 
     println!("Listening on {address}...");
