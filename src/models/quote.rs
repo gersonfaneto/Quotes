8e490f3bcf82f448e40156f091c58ee210d7f429
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-12 19:22:29 -0300

    feat(models/quote): Added new `struct Quote` for the DB model.

--- src/models/quote.rs
+++ src/models/quote.rs
@@ -1,7 +1,16 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
 pub struct CreateQuote {
     book: String,
     quote: String,
+}
+
+#[derive(Serialize)]
+pub struct Quote {
+    id: uuid::Uuid,
+    book: String,
+    quote: String,
+    inserted_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
 }
