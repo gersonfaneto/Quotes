398993298410fca5b660690a053b2aa90aae71d0
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-14 17:33:50 -0300

    docs(scripts/db): Added some comments...

--- scripts/quotes.sh
+++ scripts/quotes.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# NOTE: This script was using during tests to quickly populate the DB...
+
 xh post :8080/quotes book="The Two Towers" quote="There is some good in this world, and it’s worth fighting for."
 xh post :8080/quotes book="The Little Prince" quote="It is only with the heart that one can see rightly; what is essential is invisible to the eye."
 xh post :8080/quotes book="To Kill a Mockingbird" quote="I wanted you to see what real courage is, instead of getting the idea that courage is a man with a gun in his hand. It’s when you know you’re licked before you begin but you begin anyway and you see it through no matter what. You rarely win, but sometimes you do."
